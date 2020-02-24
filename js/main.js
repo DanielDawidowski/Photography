4df9a9f13193706f84bcc7ea2718fe9595153ea8
Daniel Dawidowski <dvds1987@gmail.com>
2020-02-24 11:56:04 +0100

    add section hero, about, service and collection

--- js/main.js
+++ js/main.js
@@ -1,35 +1,55 @@
 // Popup nav
 
 const triggers = document.querySelectorAll('.cool > li');
-    const background = document.querySelector('.dropdownBackground');
-    const nav = document.querySelector('.main-menu');
+const background = document.querySelector('.dropdownBackground');
+const nav = document.querySelector('.main-menu');
 
-    function handleEnter() {
-        this.classList.add('trigger-enter');
-        setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 150);
-        background.classList.add('open');
+function handleEnter() {
+    this.classList.add('trigger-enter');
+    setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 100);
+    background.classList.add('open');
 
-        const dropdown = this.querySelector('.dropdown');
-        const dropdownCoords = dropdown.getBoundingClientRect();
-        const navCoords = nav.getBoundingClientRect();
-        
-        const coords = {
-            height: dropdownCoords.height,
-            width: dropdownCoords.width,
-            top: dropdownCoords.top - navCoords.top,
-            left: dropdownCoords.left - navCoords.left,
-        };
+    const dropdown = this.querySelector('.dropdown');
+    const dropdownCoords = dropdown.getBoundingClientRect();
+    const navCoords = nav.getBoundingClientRect();
+    
+    const coords = {
+        height: dropdownCoords.height,
+        width: dropdownCoords.width,
+        top: dropdownCoords.top - navCoords.top,
+        left: dropdownCoords.left - navCoords.left,
+    };
 
-        background.style.setProperty('width', `${coords.width}px`);
-        background.style.setProperty('height', `${coords.height}px`);
-        background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`);
+    background.style.setProperty('width', `${coords.width}px`);
+    background.style.setProperty('height', `${coords.height}px`);
+    background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`);
 
-    } 
+} 
 
-    function handleLeave() {
-        this.classList.remove('trigger-enter', 'trigger-enter-active');
-        background.classList.remove('open');
-    }
+function handleLeave() {
+    this.classList.remove('trigger-enter', 'trigger-enter-active');
+    background.classList.remove('open');
+}
 
-    triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
-    triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
+triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
+
+// Popup nav   
+
+// // Change Nav 
+// const navChange = document.querySelector('#about');
+// const topOfNav = navChange.offsetTop;
+
+// function fixNav() {
+//     if(window.scrollY >= topOfNav) {
+//         document.body.style.paddingTop = navChange.offsetHeight + 'px';
+//         document.body.classList.add('fixed-nav');
+//     } else {
+//         document.body.style.paddingTop = 0;
+//         document.body.classList.remove('fixed-nav')
+//     }
+// }
+
+// window.addEventListener('scroll', fixNav)        
+
+// // Change Nav 
